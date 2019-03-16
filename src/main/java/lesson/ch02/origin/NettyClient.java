08a8c8f10e956457bb121246a752fdca56576a1b
Hao YANG <hyang@Haos-MacBook-Pro.local>
2019-03-16 11:15:20 +0800

    1. 新增ignore配置 2. 加入netty并运行成功

---
+++ src/main/java/lesson/ch02/origin/NettyClient.java
@@ -0,0 +1,44 @@
+package lesson.ch02.origin;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
+
+/**
+ * title: copy from 闪电侠
+ *
+ * @author Hao YANG
+ * @since 2019.03.16
+ */
+@Slf4j
+public class NettyClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        Bootstrap bootstrap = new Bootstrap();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                });
+
+        Channel channel = bootstrap.connect("127.0.0.1", 8000).channel();
+
+        while (true) {
+            String tmp = new Date() + ": hello world!";
+            log.info(tmp);
+            channel.writeAndFlush(tmp);
+            Thread.sleep(2000);
+        }
+    }
+}
