f52529273d1aeef964c507d8550aa8b91c30a5ac
Hao YANG <hyang@Haos-MacBook-Pro.local>
2019-03-22 00:31:54 +0800

    ch06 end

---
+++ src/main/java/lesson/ch06/MyNettyClientV2.java
@@ -0,0 +1,68 @@
+package lesson.ch06;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.AttributeKey;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+import java.util.Date;
+
+/**
+ * title:
+ *
+ * @author Hao YANG
+ * @since 2019.03.19
+ */
+@Slf4j
+public class MyNettyClientV2 {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+
+                            @Override
+                            public void channelActive(ChannelHandlerContext ctx) {
+                                log.info(new Date() + " client write data");
+                                // 写数据
+                                ByteBuf buffer = ctx.alloc().buffer();
+                                buffer.writeBytes("Hello World".getBytes(Charset.forName("utf-8")));
+                                // 输出
+                                ctx.channel().writeAndFlush(buffer);
+                            }
+
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                                ByteBuf response = (ByteBuf) msg;
+                                log.info(new Date() + ": client receive data from server -> " + response.toString(Charset.forName("utf-8")));
+                            }
+
+                        });
+                    }
+                })
+                .attr(AttributeKey.newInstance("clientName"), "nettyClient")
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000);
+
+        bootstrap.connect("127.0.0.1", 8090).addListener(future -> {
+            if (future.isSuccess()) {
+                log.info("连接成功!");
+            } else {
+                log.info("连接失败!");
+            }
+        });
+    }
+}
---
+++ src/main/java/lesson/ch06/MyNettyServerV2.java
@@ -0,0 +1,77 @@
+package lesson.ch06;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.AttributeKey;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+import java.util.Date;
+
+/**
+ * title:
+ *
+ * @author Hao YANG
+ * @since 2019.03.19
+ */
+@Slf4j
+public class MyNettyServerV2 {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup parent = new NioEventLoopGroup();
+        NioEventLoopGroup children = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        serverBootstrap.group(parent, children)
+                .channel(NioServerSocketChannel.class)
+                /** parent 相关 **/
+                .handler(new ChannelInitializer() {
+                    @Override
+                    protected void initChannel(Channel ch) throws Exception {
+                        log.info("parent initChannel ch: {}", ch.id());
+                    }
+                })
+                .attr(AttributeKey.newInstance("serverName"), MyNettyServerV2.class.getSimpleName())
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                /** children 相关 **/
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                                ByteBuf byteBuf = (ByteBuf) msg;
+                                log.info(new Date() + ": 服务端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
+
+                                ByteBuf response = ctx.alloc().buffer();
+                                response.writeBytes("Hi, Siri".getBytes(Charset.forName("utf-8")));
+                                ctx.channel().writeAndFlush(response);
+                            }
+
+                        });
+                    }
+                })
+                .childAttr(AttributeKey.newInstance("childServerName"), MyNettyServerV2.class.getSimpleName() + "-child")
+                .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+        bind(serverBootstrap);
+    }
+
+    private static int port = 8090;
+    private static void bind(ServerBootstrap serverBootstrap) {
+        serverBootstrap.bind(port).addListener((future)->{
+            if (future.isSuccess()) {
+                log.info("bind success port: {}", port);
+            } else {
+                log.info("bind failure port: {}", port);
+                port++;
+                bind(serverBootstrap);
+            }
+        });
+    }
+}
